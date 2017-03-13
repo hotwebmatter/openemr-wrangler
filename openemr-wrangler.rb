9c75d1d50fb6ac7b62a001c22ff644d48a719a7e
Matthew Obert <matt@hotwebmatter.com>
2017-03-13 16:37:18 -0400

    Move Mysql2::Client.new() instantiation adjacent to comment explaining hash of options

--- openemr-wrangler.rb
+++ openemr-wrangler.rb
@@ -10,15 +10,15 @@ require 'json'
 # this takes a hash of options, almost all of which map directly
 # to the familiar database.yml in rails
 # See http://api.rubyonrails.org/classes/ActiveRecord/ConnectionAdapters/MysqlAdapter.html
-
-last_index_value = 61
-
 client = Mysql2::Client.new(:host => "localhost", :port => "3306", :username => "root", :password => "rootpw", :database => "openemr")
 
 # connection = Excon.new(
 # 'http://ce018849.ngrok.io/mineblock',
 # :instrumentor => ActiveSupport::Notifications
 # )
+
+# last_index_value set manually for hackathon demo
+last_index_value = 61
 
 while true
 
